b21d9b8dc8eb28508744d676fd81a4209a9559e1
Roniel Nunes <roniel.barbosa@ufv.br>
2021-06-17 20:58:49 -0300

    remoção de comentários

--- main.cpp
+++ main.cpp
@@ -5,19 +5,6 @@
 int main(int argc, char const *argv[])
 {
     Jogo *engine = new Jogo();
-    // Tabuleiro *jogo = new Tabuleiro(); //O new faz a alocação do objeto é retorna seu endereço
-    // jogo->inicializarTabuleiro();
-    // jogo->mostrarTabuleiro();
-    // jogo->marcar(1,'x');
-    // jogo->mostrarTabuleiro();
-    // jogo->marcar(2,'x');
-    // jogo->mostrarTabuleiro();
-    // jogo->marcar(4,'x');
-    // jogo->mostrarTabuleiro();
-    // jogo->marcar(2,'0');
-    // jogo->mostrarTabuleiro();
-
-    // cout << sistema;
 
     return 0;
 }
